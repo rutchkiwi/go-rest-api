1716442e653cfdf9a71ecddf059310ac16bb535b
Viktor Holmberg <viktor.holmberg@gmail.com>
2015-06-25 14:50:12 +0100

    wip

--- webservice_test.go
+++ webservice_test.go
@@ -45,18 +45,9 @@ func TestPostAndGetUSer(t *testing.T) {
 
 	buildWebservice()
 
-	// bodyReader := strings.NewReader("<Sample><Value>42</Value></Sample>")
-	// 	httpRequest, _ := http.NewRequest("GET", "/test/THIS", bodyReader)
-	// 	httpRequest.Header.Set("Content-Type", restful.MIME_XML)
-	// 	httpWriter := httptest.NewRecorder()
-
 	bodyReader := strings.NewReader(`{"username":"viktor"}`)
 	httpRequest, _ := http.NewRequest("POST", "/", bodyReader)
 	httpRequest.Header.Set("Content-Type", restful.MIME_JSON)
-	// req := restful.NewRequest(httpRequest)
-
-	// recorder := new(httptest.ResponseRecorder)
-	// resp := restful.NewResponse(recorder)
 
 	httpWriter := httptest.NewRecorder()
 
@@ -70,17 +61,10 @@ func TestPostAndGetUSer(t *testing.T) {
 	fmt.Println("/" + location)
 
 	getHttpReq, err := http.NewRequest("GET", "/"+location, nil)
-	require.NoError(t, err, "test error")
-	// getReq := restful.NewRequest(getHttpReq)
-
-	fmt.Println("222")
+	require.NoError(t, err, "error in test")
 
 	httpWriter = httptest.NewRecorder()
-
-	fmt.Println("3333")
 	restful.DefaultContainer.ServeHTTP(httpWriter, getHttpReq)
-	fmt.Println("4444")
-
 	assert.Equal(t, 200, httpWriter.Code)
 
 }
